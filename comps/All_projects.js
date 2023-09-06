5c8064f27995a7b0666b0679b53945785ba96909
Nouran-Ali <noranali237@gmail.com>
2023-09-06 04:17:46 +0300

    finish current projects

--- comps/All_projects.js
+++ comps/All_projects.js
@@ -3,33 +3,43 @@ import Project from './Project'
 
 const All_projects = () => {
     return (
-        <div className="grid grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 gap-6">
-            <Project
-                title="Wedding hall - Palace Prince"
-                href="/"
-                icon="/../palace.png"
-                background={styles.bgProjectOne}
-                paragragh="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries."
-                iconTwo="/../palace2.png"
-            />
+        <div className={``}>
+            <div className="grid grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 gap-6 mr-7 mb-44">
+                <Project
+                    title="Wedding hall - Palace Prince"
+                    href="/wedding_hall"
+                    icon="/../palace.png"
+                    background={styles.bgProjectOne}
+                    paragragh="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries."
+                    iconTwo="/../palace2.png"
+                />
 
-            <Project
-                title="Bridal showers"
-                href="/"
-                icon="/../bathing.png"
-                background={styles.bgProjectTwo}
-                paragragh="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries."
-                iconTwo="/../bathing2.png"
-            />
+                <Project
+                    title="Bridal showers"
+                    href="/"
+                    icon="/../bathing.png"
+                    background={styles.bgProjectTwo}
+                    paragragh="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries."
+                    iconTwo="/../bathing2.png"
+                />
 
-            <Project
-                title="Private sports club"
-                href="/"
-                icon="/../treadmill.png"
-                background={styles.bgProjectThree}
-                paragragh="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries."
-                iconTwo="/../treadmill2.png"
-            />
+                <Project
+                    title="Private sports club"
+                    href="/"
+                    icon="/../treadmill.png"
+                    background={styles.bgProjectThree}
+                    paragragh="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries."
+                    iconTwo="/../treadmill2.png"
+                />
+                <Project
+                    title="Structure stage"
+                    href="/"
+                    icon="/../concrete.png"
+                    background={styles.bgProjectFour}
+                    paragragh="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries."
+                    iconTwo="/../concrete2.png"
+                />
+            </div>
         </div>
     )
 }
